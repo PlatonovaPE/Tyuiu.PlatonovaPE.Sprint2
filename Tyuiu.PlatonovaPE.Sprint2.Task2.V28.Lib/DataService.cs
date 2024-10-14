4214332313ac73a1314a7b1111f46b8d05a26899
Полина Платонова <Tyuiu.PlatonovaPE@outlook.com>
2024-10-14 16:11:02 +0500

    Task3

---
+++ Tyuiu.PlatonovaPE.Sprint2.Task2.V28.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Design;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.PlatonovaPE.Sprint2.Task2.V28.Lib
+{
+    public class DataService : ISprint2Task2V28
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+
+            if ((x >= 3) && (x <= 5) && (x >= 10) && (x <= 13) && (x >= 10) && (x <= 12) && (x >= 6) && (x <= 9) && (x >= 3) && (x <= 8) && (x >= 6) && (x <= 8) && (x >= 3) && (x <= 13) && (x >= 7) && (x <=10) && (y >= 4) && (y <= 5) && ( y >= 3) && (y <= 7) && (y >= 2) && (y <= 3) && ( y >= 4) && (y <= 5) && (y >= 6) && (y <= 7) && (y >= 8) && ( y <= 10) && ( y >= 9) && (y <= 13))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.PlatonovaPE.Sprint2.Task2.V28/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.PlatonovaPE.Sprint2.Task2.V28.Lib;
+namespace Tyuiu.PlatonovaPE.Sprint2.Task2.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Исходные данные                                                              *");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("Введите значение Х:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Результат:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
