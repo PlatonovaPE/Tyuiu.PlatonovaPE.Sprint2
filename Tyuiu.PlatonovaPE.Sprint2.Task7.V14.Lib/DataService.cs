02cee6598df78e5416955394486042230ebaee21
platonovaPE@outlook.com <parsenevv@mail.ru>
2024-10-18 12:49:01 +0500

    Task7

--- Tyuiu.PlatonovaPE.Sprint2.Task7.V14.Lib/DataService.cs
+++ Tyuiu.PlatonovaPE.Sprint2.Task7.V14.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.PlatonovaPE.Sprint2.Task7.V14.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((x > 0) && (x < 1) && (y > 0) && (y < 1)))
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((x >= 0) && (x <= 1) && (y >= 0) && (y <= 1)))
             {
                 res = true;
             }
