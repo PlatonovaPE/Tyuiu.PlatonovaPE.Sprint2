6750ffea1852ce25d02af2b7f376e3f22f561173
platonovaPE@outlook.com <parsenevv@mail.ru>
2024-10-31 12:13:42 +0500

    Task7

--- Tyuiu.PlatonovaPE.Sprint2.Task7.V14.Lib/DataService.cs
+++ Tyuiu.PlatonovaPE.Sprint2.Task7.V14.Lib/DataService.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.Design;
-using System.Timers;
-using tyuiu.cources.programming.interfaces.Sprint2;
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.PlatonovaPE.Sprint2.Task7.V14.Lib
 {
     public class DataService : ISprint2Task7V14
@@ -8,23 +6,18 @@ namespace Tyuiu.PlatonovaPE.Sprint2.Task7.V14.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((x >= 0) && (x <= 1) && (y >= 0) && (y <= 1)))
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) & (x >= 0 && y >= -1 && y <= 1))
             {
-                res = true;
+                return true;
             }
             else
             {
-                res = false;
+                if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) & (y >= 0 & y <= 1 & y >= -x))
+                {
+                    return true;
+                }
+                return false;
             }
-            if (((y == x) || (y == -x)) && ((x >= 0) && (x <= 1) && (y >= 0) && (y <= 1))) 
-            {
-                res = true;
-            }
-            else
-            {
-                res = false;
-            }
-            return res;
         }
     }
 }
