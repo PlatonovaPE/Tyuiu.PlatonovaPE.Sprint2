0a6412f22361348af66c883f04e7a3633f341d44
platonovaPE@outlook.com <parsenevv@mail.ru>
2024-11-14 14:55:38 +0500

    Task 5

--- Tyuiu.PlatonovaPE.Sprint2.Task5.V2/Program.cs
+++ Tyuiu.PlatonovaPE.Sprint2.Task5.V2/Program.cs
@@ -2,11 +2,19 @@
 using System.ComponentModel.Design;
 using Tyuiu.PlatonovaPE.Sprint2.Task5.V2.Lib;
 DataService ds = new DataService();
-
-
-Console.WriteLine("********************************************************************************");
-Console.WriteLine("* Исходные данные                                                              *");
-Console.WriteLine("********************************************************************************");
+Console.Title = "Спринт #2 | Выполнила: Платонова Полина Евгеньвна  | ИСПб-24-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #2                                                             *");
+Console.WriteLine("* Выполнила:  Платонова Полина Евгеньвна  | ИСПб-24-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("*  По данному месяцу, определите в какую пору года попадает этот месяц .    *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
 
 Console.WriteLine("Введите месяц:");
 int MonthSeason = Convert.ToInt32(Console.ReadLine());
