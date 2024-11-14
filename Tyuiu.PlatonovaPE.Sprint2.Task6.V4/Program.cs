9fb8e34f8b2dafbe2db64526f7f839089d82880a
platonovaPE@outlook.com <parsenevv@mail.ru>
2024-11-14 14:50:25 +0500

    Task 6

--- Tyuiu.PlatonovaPE.Sprint2.Task6.V4/Program.cs
+++ Tyuiu.PlatonovaPE.Sprint2.Task6.V4/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.Design;
 using Tyuiu.PlatonovaPE.Sprint2.Task6.V4.Lib;
+
 DataService ds = new DataService();
-
-
-Console.WriteLine("********************************************************************************");
-Console.WriteLine("* Исходные данные                                                              *");
-Console.WriteLine("********************************************************************************");
-
+Console.Title = "Спринт #2 | Выполнила: Платонова Полина Евгеньвна  | ИСПб-24-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #14                                                             *");
+Console.WriteLine("* Выполнила:  Платонова Полина Евгеньвна  | ИСПб-24-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программy, которая запрашивает исходные данные и вычисляет,    *");
+Console.WriteLine("* находится ли точка с координатами X,Y в заштрихованной области.         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
 Console.WriteLine("Введите число:");
 int suilt = Convert.ToInt32(Console.ReadLine());
 
