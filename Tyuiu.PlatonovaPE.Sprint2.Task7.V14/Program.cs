56461a0de762191a30e59ab950ff2ef8af62c582
platonovaPE@outlook.com <parsenevv@mail.ru>
2024-11-14 14:49:04 +0500

    Task7

--- Tyuiu.PlatonovaPE.Sprint2.Task7.V14/Program.cs
+++ Tyuiu.PlatonovaPE.Sprint2.Task7.V14/Program.cs
@@ -1,35 +1,46 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.Design;
-using Tyuiu.PlatonovaPE.Sprint2.Task7.V14.Lib;
-
-
-Console.WriteLine("********************************************************************************");
-Console.WriteLine("* Исходные данные                                                              *");
-Console.WriteLine("********************************************************************************");
-
-Console.WriteLine("Введите  значение Х:");
-double x = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Введите значение У:");
-double y = Convert.ToDouble(Console.ReadLine());
-
-DataService ds = new DataService();
-bool res = ds.CheckDotInShadedArea(x, y);
-
-
-Console.WriteLine("********************************************************************************");
-Console.WriteLine("* Результат:                                                                   *");
-Console.WriteLine("********************************************************************************");
-
-
-if (res)
+﻿using Tyuiu.PlatonovaPE.Sprint2.Task7.V14.Lib;
+namespace Tyuiu.PlatonovaPE.Sprint2.Task7.V14
 {
-    Console.WriteLine("Точно находится в заштрихованной области");
-}
-else
-{
-    Console.WriteLine("Точно не находится в заштрихованной области");
-}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила: Платонова Полина Евгеньвна  | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнила:  Платонова Полина Евгеньвна  | ИСПб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программy, которая запрашивает исходные данные и вычисляет,    *");
+            Console.WriteLine("* находится ли точка с координатами X,Y в заштрихованной области.         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
 
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
 
-Console.ReadKey();
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находиться в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находиться в заштрихованной области");
+            }
+        }
+    }
+}
